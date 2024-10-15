f1883796924d42feba8bdc4c5cab89b0427a36e7
PuliSaglio <pulisaglio@gmail>
2024-10-15 19:59:24 -0300

    Adicionado Listagem de clientes do Banco

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Banco banco1 = new Banco();		
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		List<Conta> listaContas = new ArrayList<>();
+		Cliente ignacio = new Cliente();
+		ignacio.setNome("Ignacio");
+		
+		Cliente cecilia = new Cliente();
+		cecilia.setNome("Cecilia");	
+		
+		Conta ccIgnacio = new ContaCorrente(ignacio);
+		Conta poupancaIgnacio = new ContaPoupanca(ignacio);
+		
+		Conta poupancaCeci = new ContaPoupanca(cecilia);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		listaContas.add(ccIgnacio);
+		listaContas.add(poupancaCeci);
+		listaContas.add(poupancaIgnacio);
+		banco1.setContas(listaContas);
 		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		System.out.println("NOME DOS CLIENTES DO BANCO");
+		//lambda expression + stream API para printar os clientes do banco1
+		banco1.getContas().forEach(conta -> System.out.println("Cliente: " + conta.cliente.getNome()));
+		
+		ccIgnacio.depositar(150);
+		ccIgnacio.transferir(100, poupancaCeci);
+		poupancaIgnacio.depositar(50.5);
+		
+		ccIgnacio.imprimirExtrato();
+		poupancaIgnacio.imprimirExtrato();
+		poupancaCeci.imprimirExtrato();
 	}
 
 }
